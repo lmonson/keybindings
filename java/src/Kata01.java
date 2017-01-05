0325c1f66d872f3fea0d7ed4bc12e59dc66e9b88
lmonson <lmpub01@gmail.com>
2017-01-05 14:33:32 -0700

    First kata for intellij osx 10.5+ keymap

---
+++ java/src/Kata01.java
@@ -0,0 +1,42 @@
+// http://moduscreate.com/12-intellij-idea-keyboard-shortcuts/
+
+public class Kata01 {
+    // Comment out the line below with a line comment command-/
+
+    int i = 3;
+
+    // put the cursor inside this function and select increasing blocks,    option-up
+    // then decrease block selection option-down
+
+    public void blah() {
+        if ( true==false ) {
+            foo();
+            for(int i=0; i<12;++i) {
+                foo();
+            }
+        }
+    }
+
+
+    // Comment out the function below with a block comment  shift-ctrl-/
+
+
+    public void foo() {
+        int j = 4;
+    }
+
+
+    // Delete line cmd-backspace
+
+    int foo=3;
+
+
+    // Reformat this code option-command-l
+
+    public
+        long bar(int
+                 i, long j) { return i*j; }
+
+
+
+}
